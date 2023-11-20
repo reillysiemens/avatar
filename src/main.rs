5d3cffb998099e985d9a19b3187cfc944d1226c6
Reilly Tucker Siemens <reilly@tuckersiemens.com>
2023-11-19 21:01:56 -0800

    ✨ Use ConnectInfo to get the remote IP

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use axum::{routing::get, Router};
+use axum::{extract::ConnectInfo, routing::get, Router};
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route(
+        "/",
+        get(|ConnectInfo(addr): ConnectInfo<SocketAddr>| async move {
+            format!("Hello, {}!", addr.ip())
+        }),
+    );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let make_service = app.into_make_service_with_connect_info::<SocketAddr>();
+    axum::serve(listener, make_service).await.unwrap();
 }
