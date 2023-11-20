206671a09fdfab692ea0bef306cb823d5ca3a8da
Reilly Tucker Siemens <reilly@tuckersiemens.com>
2023-11-19 20:44:04 -0800

    ✨ Add a "Hello, World!" server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
