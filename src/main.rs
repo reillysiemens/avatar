a6180529638fc2ac69fbb35f29cadb85fdfbef34
Reilly Tucker Siemens <reilly@tuckersiemens.com>
2023-11-19 21:03:45 -0800

    ♻️ Use a function handler instead of a closure

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use axum::{extract::ConnectInfo, routing::get, Router};
 use std::net::SocketAddr;
 
+async fn avatar(ConnectInfo(addr): ConnectInfo<SocketAddr>) -> String {
+    format!("Hello, {}", addr.ip())
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route(
-        "/",
-        get(|ConnectInfo(addr): ConnectInfo<SocketAddr>| async move {
-            format!("Hello, {}!", addr.ip())
-        }),
-    );
+    let app = Router::new().route("/avatar.png", get(avatar));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     let make_service = app.into_make_service_with_connect_info::<SocketAddr>();
